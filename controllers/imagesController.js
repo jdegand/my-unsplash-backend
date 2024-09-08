2516a3cb41d494c6f82e9c7d7627ec00dcdc6917
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-09-08 03:15:51 +0000

    fix: don't save create result into variable

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -7,26 +7,21 @@ const getAllImages = async (req, res) => {
     const accessToken = req.headers.authorization.split(' ')[1];
 
     jwt.verify(
-        accessToken, 
+        accessToken,
         process.env.ACCESS_TOKEN_SECRET,
         async (err, decoded) => {
             try {
-            const images = await Image.find({user: decoded.UserInfo._id});
-            if(!images) return res.status(204).json({ 'message': "No Images found" });
-            res.json(images);
-            } catch(err){
+                const images = await Image.find({ user: decoded.UserInfo._id });
+                if (!images) return res.status(204).json({ 'message': "No Images found" });
+                res.json(images);
+            } catch (err) {
                 console.log(err)
             }
         }
     )
-    /*
-    const images = await Image.find();
-    if (!images) return res.status(204).json({ 'message': 'No Images found' });
-    res.json(images);
-    */
 }
 
-const createImage = async (req,res) => {
+const createImage = async (req, res) => {
 
     const accessToken = req.headers.authorization.split(' ')[1];
 
@@ -37,28 +32,28 @@ const createImage = async (req,res) => {
 
             const { url, label } = req.body;
             if (!url || !label) return res.status(400).json({ 'message': 'Url and label are required.' });
-        
+
             // the url can be the same and the picture can be different ie https://source.unsplash.com/random
-            
+
             // The Image model had the url with unique:true  
 
             // check for duplicate images in the db
             // const duplicate = await Image.findOne({ url: url }).exec();
-        
+
             // if (duplicate) return res.sendStatus(409); // status(409).json({message: "Image already saved"})
-        
+
             try {
-                const result = await Image.create({ 
+                await Image.create({
                     "url": url,
                     "label": label,
                     "user": decoded.UserInfo._id
                 });
-        
+
                 res.status(201).json({ 'success': `New image added!` });
             } catch (err) {
                 res.status(500).json({ 'message': err.message });
             }
-         
+
         }
     );
 }
